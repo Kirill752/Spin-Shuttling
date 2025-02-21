c7967e88eeacb66189a2dda217018b2d0e938cf2
Kirill Latyshov <kil.real.latexshow@gmail.com>
2025-02-21 10:44:57 +0300

    Добавляю удаление узла

--- tree.cpp
+++ tree.cpp
@@ -7,9 +7,9 @@
 #define COLOR_RED "\033[31m"
 #define COLOR_GREEN "\033[32m"
 
-
-std::ostream& operator<<(std::ostream& stream, const Point2D& p){
-    stream << '{'<< p.x << ',' << p.y << '}';
+std::ostream &operator<<(std::ostream &stream, const Point2D &p)
+{
+    stream << '{' << p.x << ',' << p.y << '}';
     return stream;
 };
 
@@ -26,6 +26,24 @@ void freemem(treeNode *tN)
 tree::~tree()
 {
     freemem(this->root);
+}
+
+tree::tree(treeNode *L, treeNode *R)
+{
+    this->root = new treeNode((L->val + R->val) / 2);
+    this->leftEnd = L;
+    this->rightEnd = R;
+    this->root->left = this->leftEnd;
+    this->root->right = this->rightEnd;
+}
+
+tree::tree(double L, double R)
+{
+    this->root = new treeNode((L + R) / 2);
+    this->leftEnd = new treeNode(L);
+    this->rightEnd = new treeNode(R);
+    this->root->left = this->leftEnd;
+    this->root->right = this->rightEnd;
 }
 
 void tree::addPoint(double val)
@@ -46,6 +64,14 @@ void tree::addPoint(double val)
             }
             if (curruntNode->val < val)
             {
+                if (curruntNode->left == this->leftEnd)
+                {
+                    treeNode *buf = this->leftEnd;
+                    curruntNode->left = new treeNode(val, curruntNode);
+                    curruntNode->left->left = buf;
+                    return;
+                }
+
                 if (curruntNode->left == NULL)
                 {
                     curruntNode->left = new treeNode(val, curruntNode);
@@ -56,6 +82,13 @@ void tree::addPoint(double val)
             }
             if (curruntNode->val > val)
             {
+                if (curruntNode->right == this->rightEnd)
+                {
+                    treeNode *buf = this->rightEnd;
+                    curruntNode->right = new treeNode(val, curruntNode);
+                    curruntNode->right->right = buf;
+                    return;
+                }
                 if (curruntNode->right == NULL)
                 {
                     curruntNode->right = new treeNode(val, curruntNode);
@@ -89,9 +122,53 @@ treeNode *tree::findPoint(double val)
     return curruntNode;
 }
 
-void printTree(treeNode* root, const std::string& prefix, bool isLeft)
+void tree::deletePoint(double val) {
+    treeNode *curentNode = this->findPoint(val);
+    // Если у узда нет потомков, то просто удаляем его
+    if (curentNode->left == nullptr && curentNode->right == nullptr)
+    {
+        if (curentNode->root->left == curentNode){
+            curentNode->root->left = nullptr;
+        }
+        else {
+            curentNode->root->right = nullptr;
+        }   
+        curentNode->~treeNode();
+        return;
+    }
+    // Если у узла один потомок, то на место текущего узла ставим его потомка
+    if (curentNode->left != nullptr)
+    {
+        if (curentNode->root->left == curentNode){
+            curentNode->root->left = curentNode->left;
+        }
+        else {
+            curentNode->root->right = curentNode->left;
+        }
+        curentNode->~treeNode();
+        return;
+    }
+    if (curentNode->right != nullptr)
+    {
+        if (curentNode->root->left == curentNode){
+            curentNode->root->left = curentNode->right;
+        }
+        else {
+            curentNode->root->right = curentNode->right;
+        }
+        curentNode->~treeNode();
+        return;
+    }
+    // Если у узла два потомка, то ищем узел с минимальным значением в правом поддереве этого узла
+    treeNode *toReplace = curentNode->right;
+    for (; toReplace->left != nullptr; toReplace = toReplace->left) {
+    }
+}
+
+void printTree(treeNode *root, const std::string &prefix = "", bool isLeft = true)
 {
-    if (root == nullptr) return;
+    if (root == nullptr)
+        return;
     // Выводим текущий узел
     std::cout << prefix;
     std::cout << (isLeft ? "┌──" : "└──");
--- tree.h
+++ tree.h
@@ -18,21 +18,38 @@ struct treeNode
                                           right(right)
     {
     }
+    ~treeNode() {
+        if (root != nullptr)
+        {
+            delete root;
+        }
+        if (left != nullptr){
+            delete left;
+        }
+        if (right != nullptr)
+        {
+            delete right;
+        }   
+    }
 };
 
 class tree
 {
 public:
     treeNode *root;
+    treeNode *leftEnd;
+    treeNode *rightEnd;
     tree(treeNode *root) : root(root) {}
     tree(double val)
     {
         this->root = new treeNode(val);
     }
+    tree(treeNode *L, treeNode *R);
+    tree(double L, double R);
     ~tree();
     void addPoint(double val);
     treeNode *findPoint(double val);
     void deletePoint(double val);
 };
-void printTree(treeNode *root, const std::string &prefix = "", bool isLeft = true);
+void printTree(treeNode *root, const std::string &prefix, bool isLeft);
 #endif
